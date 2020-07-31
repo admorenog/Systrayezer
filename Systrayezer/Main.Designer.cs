9f04b46d11d95a40ff8c8a517d455a42294f4135
amg <adrian.moreno.ext@rsi.cajarural.com>
2020-07-31 07:38:13 +0200

    First commit with a concept

---
+++ Systrayezer/Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Systrayezer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            this.Main_Dispose(disposing);
+            /*
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            */
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnApplyHotkeys = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Systrayezer";
+            this.trayIcon.Visible = true;
+            this.trayIcon.DoubleClick += new System.EventHandler(this.Tray_doubleclick);
+            // 
+            // btnApplyHotkeys
+            // 
+            this.btnApplyHotkeys.Location = new System.Drawing.Point(450, 315);
+            this.btnApplyHotkeys.Name = "btnApplyHotkeys";
+            this.btnApplyHotkeys.Size = new System.Drawing.Size(125, 30);
+            this.btnApplyHotkeys.TabIndex = 0;
+            this.btnApplyHotkeys.Text = "Apply hotkeys";
+            this.btnApplyHotkeys.UseVisualStyleBackColor = true;
+            this.btnApplyHotkeys.Click += new System.EventHandler(this.btnApplyHotkeys_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 353);
+            this.Controls.Add(this.btnApplyHotkeys);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Systrayezer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Main_Disponse(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.Button btnApplyHotkeys;
+    }
+}
+
---
+++ Systrayezer/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Systrayezer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Main_Dispose(bool disposing)
+        {
+            this.Visible = false;
+        }
+        
+        private void Tray_doubleclick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+        }
+
+        private void btnApplyHotkeys_Click(object sender, EventArgs e)
+        {
+            var keyboardHookManager = new KeyboardHookManager();
+            keyboardHookManager.Start();
+        }
+    }
+}
